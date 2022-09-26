525781a71ac446e0e458334bb53a7efdb25db360
Ather1230 <ather.biswas0057@gmail.com>
2022-09-26 13:50:41 +0600

    set locla storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summery</h1>
             <p>Selected items: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
